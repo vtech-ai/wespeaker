8b41eab4b393ef44fc74990dca93df2b3b1544f1
xj <xiaojun.zhang@nyonic.com>
2025-06-17 11:06:01 +0800

    test runnable, failed

--- service/server.py
+++ service/server.py
@@ -12,23 +12,19 @@ import uuid
 import json
 from dotenv import load_dotenv
 
-
 app = FastAPI(title="WeSpeaker API")
 
 load_dotenv()
 
-BUCKET_NAME = "vtech-prod"
+BUCKET_NAME = os.getenv("OSS_BUCKET_NAME")
 OSS_ACCESS_KEY_ID = os.getenv("OSS_ACCESS_KEY_ID")
 OSS_ACCESS_KEY_SECRET = os.getenv("OSS_ACCESS_KEY_SECRET")
 OSS_ENDPOINT = os.getenv("OSS_ENDPOINT")
-_oss_client = oss2.OSSClient(
-        access_key_id=OSS_ACCESS_KEY_ID,
-        access_key_secret=OSS_ACCESS_KEY_SECRET,
-        endpoint=OSS_ENDPOINT,
-        bucket_name=BUCKET_NAME
-    )
 
-VOICE_FILE = "/data/voice_file"
+_auth   = oss2.Auth(OSS_ACCESS_KEY_ID, OSS_ACCESS_KEY_SECRET)
+bucket = oss2.Bucket(_auth, OSS_ENDPOINT, BUCKET_NAME)
+
+VOICE_FILE = "./data/voiceprint.json"
 
 model = load_model("chinese")
 
@@ -43,7 +39,9 @@ class Segments(BaseModel):
 def load_user_voices(filepath: str) -> dict[str, List[float]]:
     with open(filepath, 'r', encoding='utf-8') as f:
         user_voices = json.load(f)
-    for user, vals in user_voices.items():
+    # 文件是 []  或者空
+    print(user_voices)
+    for user, vals in user_voices:
         user_voices[user] = [float(v) for v in vals]
     return user_voices
 
@@ -54,7 +52,7 @@ def persist_user_voices(filepath: str, user_voices: dict):
 user_voice_dict = load_user_voices(VOICE_FILE)
 
 @app.post("/calculate_similarities")
-async def calculate_similarities(
+def calculate_similarities(
     audio: UploadFile = File(...),
     segments: Segments = ...,
     user_id: str = ...
@@ -68,8 +66,9 @@ async def calculate_similarities(
     else:
         try:
             tmp_filename = tmp_wav_filepath()
+            audio_data = audio.file.read()
             with open(tmp_filename, "wb") as f:
-                f.write(await audio.read())
+                f.write(audio_data)
             ref_embedding = user_voice_dict[user_id]
             segments_list = [(seg.start, seg.end) for seg in segments.segments]
             similarities = model.compute_similarity_by_segments(tmp_filename, segments_list, ref_embedding)
@@ -87,23 +86,29 @@ def register_voiceprint(
     if not audio_osskey or not user_id:
         return JSONResponse({"success": False, "message": "input error"}, status_code=400)
     try:
-        response = _oss_client.get_object(audio_osskey)
+        response = bucket.get_object(audio_osskey)
         audio_data = response.read()
         filename = tmp_wav_filepath()
         with open(filename, "wb") as f:
             f.write(audio_data)
+        print(f"extracting embedding from file: {filename}, size: {len(audio_data)} bytes")
+        print(f"os.getcwd(): {os.getcwd()}")
         embedding = model.extract_embedding(filename)
+        print(f"embedding: {embedding}")
     except Exception as e:
+        print(f"error: {e}")
         return JSONResponse({"error": str(e)}, status_code=400)
-    finally:
-        os.remove(filename)
+    #finally:
+        #os.remove(filename)
     user_voice_dict[user_id] = embedding
     persist_user_voices(VOICE_FILE, user_voice_dict)
     return {"success": True}
 
 def tmp_wav_filepath():
-    return f"/tmp/tmp_{uuid.uuid4()}.wav"
+    return f"./tmp/tmp_{uuid.uuid4()}.wav"
 
 if __name__ == "__main__":
+    if not os.path.exists("./tmp"):
+        os.makedirs("./tmp")
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000) 
+    uvicorn.run(app, host="0.0.0.0", port=13700) 
